4b972b0c2c5db7823b37d1dd760be0d69194dd1b
Dwayne Williams <dwaynewillmakeit@gmail.com>
2022-02-14 08:10:35 -0500

    Stubbed out the Home, About and NotFound pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const About = () => {
+	return (
+		<>
+			<h1 className="text-6xl mb-4">Github Finder</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pulvinar
+				diam eu lectus blandit, eget sollicitudin leo vulputate. Pellentesque eu
+				vulputate enim, eu sagittis eros. Proin feugiat hendrerit eros id
+				varius. Maecenas diam risus, posuere vitae lectus eget, euismod
+				sollicitudin nulla. Suspendisse tempus, velit vitae aliquet malesuada,
+				enim orci congue ante, non sodales nulla leo id mi. Fusce lacus ante,
+				lobortis sit amet quam quis, euismod rhoncus arcu. Quisque suscipit
+				sapien risus, vel rhoncus turpis feugiat vitae.
+			</p>
+			;
+		</>
+	);
+};
+
+export default About;
