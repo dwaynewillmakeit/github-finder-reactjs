b29f6ba8af9976e1cb6ae9b3d3758e1ae877e375
Dwayne Williams <dwaynewillmakeit@gmail.com>
2022-02-17 19:45:00 -0500

    Move search users to an Action file

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -17,28 +17,7 @@ export const GithubProvider = ({children})=>{
 
     const [state, dispatch] = useReducer(githubReducer,initialState);
 
-    //Search users
-    const searchUsers = async (text) => {
-
-        setLoading();
-
-        const params = new URLSearchParams({
-            q: text
-        })
-
-		const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-			headers: {
-				Authorization: `token ${GITHUB_TOKEN}`,
-			},
-		});
-
-		const {items} = await response.json();
-		
-        dispatch({
-            type: 'GET_USERS',
-            payload: items
-        })
-	};
+    
 
     //Get a single user
     const getUser = async (login) => {
@@ -103,11 +82,8 @@ export const GithubProvider = ({children})=>{
     }
 
     return <GithubContext.Provider value={{
-        users: state.users,
-        loading: state.loading,
-        user: state.user,
-        repos: state.repos,
-        searchUsers,
+        ...state,
+        dispatch,
         getUser,
         clearUsers,
         getUserRepos
